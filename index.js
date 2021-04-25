f1ee7e31f94779f708583a1d26a6ede277394225
Alex M <>
2021-04-25 22:06:31 +0000

    Bot can now take commands, but doesn't have any yet

--- index.js
+++ index.js
@@ -27,11 +27,31 @@ for (var file of commandFiles) {
 client.on("ready", () => {
 	console.log(`${client.user.tag} is online and ready for action!\n`);
 
+	client.user.setPresence({activity: { type: "PLAYING", name: `Prefix: ${prefix} | Just online, no commands set up yet ` }, status: "dnd"});
+
 });
 
 //Commands
 client.on("message", (message) => {
+	
+	//Disregard if it was sent by a bot
+	if(message.author.bot) return;
+	//Disregard if it doesn't start with our prefix
+	if(!message.content.toLowerCase().startsWith(prefix)) return;
 
+	//Parse out the commands and arguments
+	var args = message.content.slice(prefix.length).trim().split(" ");
+	var command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command))
+		return message.reply(`I don't have a \`${command}\` command. Are you sure you spelled it right? Try using \`${prefix}commands\` to see all my commands.`);
+	try {
+		client.commands.get(command).execute(client.user, message, args, db);
+	} catch (error) {
+		console.error(error);
+		message.reply(`there was an error trying to execute that command:\n\`\`\`diff\n+> ${message.cleanContent}\n-> ${error.name}: ${error.message.split('\n')}\`\`\``);
+	}
+	
 });
 
 //This line should always be the last one in the code
