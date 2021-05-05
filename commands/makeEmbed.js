be62e280dccb4668e3e701d6877f11982b0dac05
Alex M <weirdalex0302@gmail.com>
2021-05-04 21:58:14 -0400

    And added editEmbed

--- commands/makeEmbed.js
+++ commands/makeEmbed.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 
 module.exports = {
 	name: "makeEmbed",
-	description: 'Sends a new embed to the specified channel. The embed is generated from JSON, c which you can get from an embed generator like [this one](https://embedbuilder.nadekobot.me).',
+	description: 'Sends a new embed to the specified channel. The embed is generated from JSON, which you can get from an embed generator like [this one](https://embedbuilder.nadekobot.me).',
 	usage: "makeEmbed <channel> <json>",
 	execute: async (bot, message, args, db) => {  /* lgtm [js/unused-local-variable] */ // jshint ignore:line
 		var channels = message.mentions.channels.array();
